927729ede6b50a61be9721f376bc6ca6572b819d
xali77 <72192661+xali77@users.noreply.github.com>
2020-10-04 19:43:31 +0530

    Add files via upload

---
+++ R2.R
@@ -0,0 +1,16 @@
+---
+  title: "Video Games Sales analysis"
+output: 
+  html_document: 
+  fig_height: 4
+highlight: pygments
+theme: spacelab
+---
+  ### Reg. No: 18BCE1327
+  
+  ### Name:Mohammed Ali
+  
+  ### REVIEW 2
+```{r}
+
+```
